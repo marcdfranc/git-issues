d8d8052d9d2375536ea7fc6b26b7d292c76d462b
Marcelo <marcdfranc@gmail.com>
2025-04-30 23:47:11 +0100

    kick off List

---
+++ features/issue/print.go
@@ -0,0 +1,27 @@
+package issue
+
+import (
+	"fmt"
+	"io"
+
+	"git-issues/domain"
+)
+
+const (
+	strIssueFormat = "#%v - %s (%s)\n"
+)
+
+func PrintIssues(w io.Writer, issues map[string]domain.Issue) error {
+	_, err := fmt.Fprintln(w, "\nIssues:")
+	if err != nil {
+		return err
+	}
+
+	for _, i := range issues {
+		_, err = fmt.Fprintf(w, strIssueFormat, i.Number, i.Title, i.State)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
